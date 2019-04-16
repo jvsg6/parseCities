366bd28b2566fda4f2f6f5eb5253432d8dc3f502
jvsg6 <ilichevea@ibrae.ac.ru>
2019-04-16 19:47:21 +0300

    add city class and parse separate city

--- parse_city.py
+++ parse_city.py
@@ -5,10 +5,27 @@ import os
 import bs4
 import requests
 
+class CityData:
+
+    def __init__(self):
+        self.name = ""
+        self.population = -9999
+        self.lat = -9999
+        self.lon = -9999
+
+    def __str__(self):
+        s = ""
+        s += "City name: {0}\n".format(self.name)
+        s += "\tlon: {0}\n".format(self.lon)
+        s += "\tlat :{0}\n".format(self.lat)
+        s += "\tpopulation: {0}\n".format(self.population)
+        return s
+
 
 def getCityName(pageData):
-    h1=pageData.select('h1')
+    h1=pageData.select('h3')
     cityName=h1[0].getText()
+    cityName = cityName.replace(" ", "").split(":")[-1]
     return cityName
 
 def getCityCoord(pageData):
@@ -21,20 +38,26 @@ def getCityPopulation(pageData):
     coordLine = pageData.find("table").find_all('td')
     populationSetting = coordLine[11].getText()
     population = float(populationSetting.replace(" ", "").split(":")[-1])
-    print population
     return population
 
 
 def parseCityData(pathToPage):
     s=requests.get(pathToPage)
     pageData=bs4.BeautifulSoup(s.text, "html.parser")
-    cityName = getCityName(pageData)
+    currCity = CityData()
+    currCity.name = getCityName(pageData)
     cityCoord = getCityCoord(pageData)
-    cityPop = getCityPopulation(pageData)
+    currCity.lat = cityCoord["lat"]
+    currCity.lon = cityCoord["lon"]
+    currCity.population = getCityPopulation(pageData)
+    return currCity
 
-
+def parseLetterData(pathToLetter):
+    linesInTable = pageData.find("table").find_all('td')
+    return
 
 def main():
+    parseLetterData("http://fallingrain.com/world/TU/32/a/R/")
     parseCityData('http://fallingrain.com/world/TU/78/Sahinler_Mahallesi.html')
 
     print "Done!"
